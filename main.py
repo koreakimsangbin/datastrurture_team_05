ccc4448b926ce071cd81b3670ad6d6cba4066f7b
kimsangbin <rlatkdqls53@korea.ac.kr>
2023-04-03 00:44:22 +0900

    test

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+def comparison(linear_point, count):
+    class graph_comparison():
+        if linear_point > count:
+            print(count)
+
+
+student_point = {}
+linear_point = {}
+discrete_point = {}
+count = 0
+
