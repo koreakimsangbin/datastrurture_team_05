4004429de562c3e27bf7ace1a903fc900b1fd84b
koreakimsangbin <rlatkdqls53@korea.ac.kr>
2023-04-19 15:29:38 +0900

    code 1 kim sang bin

--- main.py
+++ main.py
@@ -1,11 +1,59 @@
-def comparison(linear_point, count):
-    class graph_comparison():
-        if linear_point > count:
-            print(count)
+survey_point = 0
+do_survey = 0
+freq = 1
+eat_existen = 1
+
+korean_style_food = 1
+chinese_style_food = 2
+western_style_food = 3
+taiwan_style_food = 4
+
+if (survey_point != 0):
+    do_survey += 1
+
+while (do_survey):
+    print(freq, "time to use this program \n")
+    search_food_style = int(
+        input("what kind of style food do you need: 1.korean food 2. chinese food 3. western food 4. taiwan food \n"))
+    if (search_food_style == 1):
+        print("korean food")
+        eat_existen = int(input("Did you eat it: 1. Yes 2. No"))
+        if (eat_existen == 1):
+            print("eat")
+            eat_existen += 1
+        else:
+            print("no eat")
 
 
-student_point = {}
-linear_point = {}
-discrete_point = {}
-count = 0
+    elif (search_food_style == 2):
+        print("chinese food")
+        eat_existen = int(input("Did you eat it: 1. Yes 2. No"))
+        if (eat_existen == 1):
+            print("eat")
+            eat_existen += 1
+        else:
+            print("no eat")
+
+    elif (search_food_style == 3):
+        print("western food")
+        eat_existen = int(input("Did you eat it: 1. Yes 2. No"))
+        if (eat_existen == 1):
+            print("eat")
+            eat_existen += 1
+        else:
+            print("no eat")
+
+    elif (search_food_style == 4):
+        print("taiwan food")
+        eat_existen = int(input("Did you eat it: 1. Yes 2. No"))
+        if (eat_existen == 1):
+            print("eat")
+            eat_existen += 1
+        else:
+            print("no eat")
+
+    exit_code = int(input("Do you want to exit this progran: 1. Yes 2. No"))
+    if (exit_code == 1):
+        break
+
 
