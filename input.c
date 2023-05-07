110de7f3ab4db0a12ed5409b42f608df5d8a410b
eoxsau <127696879+eoxsau@users.noreply.github.com>
2023-05-08 07:22:01 +0900

    input

---
+++ input.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "save.h"
+#include "input.h"
+
+void *input() {
+    char buffer[MAX_LENGTH];
+    char *text = NULL;
+    size_t text_len = 0;
+
+    printf("텍스트를 입력하세요 (여러 줄 입력 가능, 끝내려면 스페이스바를 두 번 누르고 엔터):\n");
+
+    while (fgets(buffer, MAX_LENGTH, stdin) != NULL) {
+        if (strcmp(buffer, "  \n") == 0) {
+            break;
+        }
+
+        size_t buffer_len = strlen(buffer);
+        text = (char *)realloc(text, text_len + buffer_len + 1);
+        if (text == NULL) {
+            printf("메모리를 할당하는데 실패했습니다.\n");
+            exit(1);
+        }
+
+        strcpy(text + text_len, buffer);
+        text_len += buffer_len;
+    }
+}
+
+void name() {
+    char buffer[MAX_LENGTH];
+    char *text = NULL;
+    if (text != NULL) {
+        printf("파일 이름을 입력하세요 (예: newfile.txt):\n");
+        fgets(buffer, MAX_LENGTH, stdin);
+
+        save_to_file(buffer, text);
+        free(text);
+
+        printf("파일이 저장되었습니다: %s\n", buffer);
+    } else {
+        printf("텍스트를 입력하지 않았습니다.\n");
+    }
+}
