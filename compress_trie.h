513169d27a52a63df34801f8c1e825e4f6db605e
jgz-wone <127703544+jgz-wone@users.noreply.github.com>
2023-05-12 23:26:08 +0900

    youjiwon_compressed_trie

--- compress_trie.h
+++ compress_trie.h
@@ -1,51 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define ALPHABET 26
 
-typedef struct TrieNode {
-    struct TrieNode* children[ALPHABET];
-    char character;
-    int end_point;
-} TrieNode;
+#define MAX_ALP 30
 
-TrieNode* createNode(char character) {
-    TrieNode* node = (TrieNode*)malloc(sizeof(TrieNode));
-    for (int i = 0; i < ALPHABET; i++) {
-        node->children[i] = NULL;
+struct TrieNode {
+    int word_end;
+    struct TrieNode *child[MAX_ALP];
+};
+
+struct CompressedTrie {
+    struct TrieNode *root;
+};
+
+struct TrieNode *create_trie_node(void) {
+    struct TrieNode *node = NULL;
+    node = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+    if (node) {
+        node->word_end = 0;
+        for (int i = 0; i < MAX_ALP; i++)
+            node->child[i] = NULL;
     }
-    node->character = character;
-    node->end_point = 0;
     return node;
 }
 
-void node_insert(TrieNode* root, char* word) {
-    TrieNode* curr = root;
-    int len = strlen(word);
-
-    for (int i = 0; i < len; i++) {
-        int index = word[i] - 'a';
-        if (curr->children[index] == NULL) {
-            curr->children[index] = createNode(word[i]);
-        }
-        curr = curr->children[index];
-    }
-
-    curr->end_point = 1;
+struct CompressedTrie *create_compressed_trie(void) {
+    struct CompressedTrie *trie = NULL;
+    trie = (struct CompressedTrie *)malloc(sizeof(struct CompressedTrie));
+    if (trie)
+        trie->root = create_trie_node();
+    return trie;
 }
 
-void save_file(TrieNode* root, FILE* file, char* sentence, int level) {
-    if (root == NULL) {
-        return;
-    }
+void insert(struct CompressedTrie *trie, char *key) {
+    int length = strlen(key);
+    int index;
+    struct TrieNode *node = trie->root;
 
-    sentence[level] = root->character;
-    if (root->end_point) {
-        sentence[level + 1] = '\0';
-        fprintf(file, "%s\n", sentence);
+    for (int level = 0; level < length; level++) {
+        index = key[level] - 'a';
+        if (!node->child[index]) {
+            node->child[index] = create_trie_node();
+        }
+        node = node->child[index];
     }
+    node->word_end = 1;
+}
 
-    for (int i = 0; i < ALPHABET; i++) {
-        save_file(root->children[i], file, sentence, level + 1);
+int search(struct CompressedTrie *trie, char *key) {
+    int length = strlen(key);
+    int index;
+    struct TrieNode *node = trie->root;
+
+    for (int level = 0; level < length; level++) {
+        index = key[level] - 'a';
+        if (!node->child[index])
+            return 0;
+        node = node->child[index];
     }
+    
+    return (node != NULL && node->word_end);
+}
+
+int main(void) {
+    struct CompressedTrie *trie = create_compressed_trie();
+
+    char keys[][8] = {"apple", "banana", "grape", "orange", "hypeboy", "newjeans"};
+    int n = sizeof(keys) / sizeof(keys[0]);
+
+    for (int i = 0; i < n; i++)
+        insert(trie, keys[i]);
+
+    printf("%s", search(trie, "hypeboy") ? "Yes" : "No");
+    printf("\n");
+    printf("%s", search(trie, "pepsi") ? "Yes" : "No");
+    return 0;
 }
