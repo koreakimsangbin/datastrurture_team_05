11807759e491d7573e688eaa9d175534c9357c6a
jgz-wone <127703544+jgz-wone@users.noreply.github.com>
2023-05-18 13:10:08 +0900

    Youjiwon_Update_compress_algorithm.h

--- compress_algorithm.h
+++ compress_algorithm.h
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "trie.h"
 
+/*
 TrieNode* createCompressedTrie(TrieNode* root) {
     TrieNode* compressedRoot = createNode();
 
@@ -23,4 +24,29 @@ TrieNode* createCompressedTrie(TrieNode* root) {
 
     return compressedRoot;
 }
+*/
 
+//try_1
+char* compressT(struct TrieNode* root, char* string) {
+    struct TrieNode* R = root;
+    int count = 0;
+    int length = sizeof(string);
+    char* compressed_string = (char*)malloc(length * 2 * sizeof(char));
+    int index = 0;
+
+
+    for (int i = 0; i < length; i++) {
+        for (int j = 'A'; j <= 'Z'; j++) {
+            if (string[i] == string[i + 1])
+                count++;
+        }
+        if (count == 0)
+            compressed_string[index++] = string[i];
+        //return string;
+        else
+            compressed_string[index++] = strcat(string[i], (char)count);
+
+        compressed_string[index] = '\0';
+        return compressed_string;
+    }
+}
