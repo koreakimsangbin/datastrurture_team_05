8c782b4f8b9ab637cb27a16268b2334fb29ae54c
koreakimsangbin <rlatkdqls53@korea.ac.kr>
2023-05-26 22:57:23 +0900

    sangbinkim_final_v1

--- compress_algorithm.h
+++ compress_algorithm.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "trie.h"
+#include "compress_node.h"
 
 TrieNode* createCompressedTrie(TrieNode* root) {
     TrieNode* compressedRoot = createNode();
@@ -9,18 +10,36 @@ TrieNode* createCompressedTrie(TrieNode* root) {
     
     for (int i = 0; i < 26; i++) {
         if (root->children[i] != NULL) {
-            TrieNode* node = createNode();
+            TrieNode* node = createCompressedTrie(root->children[i]); 
             node->isEndOfWord = root->children[i]->isEndOfWord;
             compressedRoot->children[i] = node;
         }
     }
 
-     for (int i = 0; i < 26; i++) {
-        if (root->children[i] != NULL) {
-            compressedRoot->children[i] = createCompressedTrie(root->children[i]);
-        }
+    if (root->word != NULL) {
+        compressedRoot->word = (char*)malloc(strlen(root->word) + 1);
+        strcpy(compressedRoot->word, root->word);
     }
 
     return compressedRoot;
 }
+
+void printTrie(TrieNode* root, int level) {
+    if (root == NULL)
+        return;
+
+    for (int i = 0; i < level; i++) {
+        printf("  ");
+    }
+
+    if (root->word != NULL)
+        printf("%s\n", root->word);
+    else
+        printf("-\n");
+
+    for (int i = 0; i < 26; i++) {
+        printTrie(root->children[i], level + 1);
+    }
+}
+
 
